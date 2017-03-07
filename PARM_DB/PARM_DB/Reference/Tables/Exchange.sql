b88db55653bc1e8aa63443a70869974f257b0915
Aaron Jackson <aaron@barkingcat.com.au>
2017-03-08 08:16:47 +1100

    missed file

---
+++ PARM_DB/PARM_DB/Reference/Tables/Exchange.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Reference].[Exchange]
+(
+    [ExchangeID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ExchangeName] NVARCHAR(255) NOT NULL, 
+    [ExchangeCode] NCHAR(10) NOT NULL, 
+    [CountryID] INT NOT NULL, 
+    CONSTRAINT [FK_Exchange_Country] FOREIGN KEY ([CountryID]) REFERENCES [Reference].[Country]([CountryID])
+)
