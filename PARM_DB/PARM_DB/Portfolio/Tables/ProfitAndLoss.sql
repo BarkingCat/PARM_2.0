839d8d1b5abf40579763e9b8c1638abf370955d5
Aaron Jackson <aaron@barkingcat.com.au>
2017-03-09 08:21:56 +1100

    updated pnl table

--- PARM_DB/PARM_DB/Portfolio/Tables/ProfitAndLoss.sql
+++ PARM_DB/PARM_DB/Portfolio/Tables/ProfitAndLoss.sql
@@ -1,11 +1,17 @@
 ﻿CREATE TABLE [Portfolio].[ProfitAndLoss] (
     [DateID] INT NOT NULL, 
     [PortfolioID] INT NOT NULL, 
-    [StockID] INT NOT NULL, 
+    [StockID] INT NOT NULL,
+     
     [DailyLotsChange] INT NOT NULL, 
     [MTDLotsChange] INT NOT NULL, 
     [YTDLotsChange] INT NOT NULL,
     [LTDLotsChange] INT NOT NULL,
+
+    [DailyDividendsExpectedChange] DECIMAL NOT NULL, 
+    [MTDDividendsExpectedChange] DECIMAL NOT NULL, 
+    [YTDDividendsExpectedChange] DECIMAL NOT NULL,
+    [LTDDividendsExpectedChange] DECIMAL NOT NULL, 
 
     [DailyValueChange] DECIMAL NOT NULL, 
     [MTDValueChange] DECIMAL NOT NULL, 
@@ -20,7 +26,8 @@
     [DailyFundingCostChange] DECIMAL NOT NULL, 
     [MTDFundingCostChange] DECIMAL NOT NULL, 
     [YTDFundingCostChange] DECIMAL NOT NULL,
-    [LTDFundingCostChange] DECIMAL NOT NULL, 
+    [LTDFundingCostChange] DECIMAL NOT NULL,
+     
     CONSTRAINT [PK_ProfitAndLoss] PRIMARY KEY ([DateID], [PortfolioID], [StockID]), 
     CONSTRAINT [FK_ProfitAndLoss_Date] FOREIGN KEY ([DateID]) REFERENCES [Reference].[Date]([DateID]), 
     CONSTRAINT [FK_ProfitAndLoss_PortfolioDetails] FOREIGN KEY ([PortfolioID]) REFERENCES [Portfolio].[PortfolioDetails]([PortfolioID]), 
